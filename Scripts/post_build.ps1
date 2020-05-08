0242165be62d515c05937d9b8624210f526702b6
Ioannis G <john@gamban.com>
2020-05-08 18:22:33 +0300

    bump

--- Scripts/post_build.ps1
+++ Scripts/post_build.ps1
@@ -6,10 +6,10 @@ param(
 Write-Host "Config: $config"
 
 function Build-Version {
-	if ([string]::IsNullOrEmpty($env:APPVEYOR_BUILD_VERSION)) {
+	if ([string]::IsNullOrEmpty($env:launcherVersion)) {
 		$v = (Get-Command ${TargetPath}).FileVersionInfo.FileVersion
 	} else {
-        $v = $env:APPVEYOR_BUILD_VERSION
+        $v = $env:launcherVersion
     }
 
     Write-Host "Build Version: $v"
